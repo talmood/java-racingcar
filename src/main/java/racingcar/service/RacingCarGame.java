2ab61e39f368a4c76f443e2eb7ba3503e56d55fe
lgm1007 <flxl654@gmail.com>
2024-04-23 23:01:47 +0900

    feat: 레이싱 경주 클래스 RacingCarGame 추가  - 자동차를 조건에 맞게 전진하거나 STOP 하는 메서드  - 최종 우승 자동차 출력 메서드

---
+++ src/main/java/racingcar/service/RacingCarGame.java
@@ -0,0 +1,84 @@
+package racingcar.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingCarGame {
+	private static final int MIN_RANGE_NUMBER = 0;
+	private static final int MAX_RANGE_NUMBER = 9;
+	private static final int MOVING_FORWARD = 4;
+
+	private List<Car> cars = new ArrayList<>();
+
+	/**
+	 * 경주에 참가할 자동차를 등록한다.
+	 * @param car
+	 */
+	public void addRacingCar(final Car car) {
+		cars.add(car);
+	}
+
+	/**
+	 * 랜덤 값을 구한 후, MOVING_FORWARD 값 이상이면 전진시킨다.
+	 */
+	public void moveOrStop() {
+		for (Car car : cars) {
+			int random = Randoms.pickNumberInRange(MIN_RANGE_NUMBER, MAX_RANGE_NUMBER);
+			if (random >= MOVING_FORWARD) {
+				car.increasePosition();
+			}
+		}
+
+		printRacingState();
+	}
+
+	/**
+	 * 현재 자동차들의 상태를 출력한다.
+	 */
+	private void printRacingState() {
+		for (Car car : cars) {
+			car.printState();
+		}
+		System.out.println();
+	}
+
+	/**
+	 * 최종 우승 자동차를 출력한다.
+	 */
+	public void printWinner() {
+		int bestPosition = fetchBestPosition();
+		List<Car> winners = fetchWinners(bestPosition);
+
+		String winnerNames = winners.stream()
+						.map(Car::fetchCarName)
+						.collect(Collectors.joining(", "));
+
+		System.out.println("최종 우승자 : %s".formatted(winnerNames));
+	}
+
+	/**
+	 * 자동차들 중 제일 많이 전진한 길이를 구한다.
+	 * @return
+	 */
+	private int fetchBestPosition() {
+		return cars.stream()
+				.map(Car::fetchCurrentPositionLength)
+				.max(Integer::compareTo)
+				.get();
+	}
+
+	/**
+	 * 우승 자동차들을 구한다.
+	 * @param bestPosition
+	 * @return
+	 */
+	private List<Car> fetchWinners(final int bestPosition) {
+		return cars.stream()
+				.filter(car -> car.fetchCurrentPositionLength() == bestPosition)
+				.toList();
+	}
+}
