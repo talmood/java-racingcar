98a4c4d0b784a1550461a631d4b96d0bea6b7640
lgm1007 <flxl654@gmail.com>
2024-04-23 21:49:02 +0900

    feat: 레이싱 경주 자동차 Car 클래스 추가  - 자동차 객체 자신의 현재 전진 상태를 출력하는 메서드  - 자동차의 현재 전진 위치를 구하는 메서드  - 자동차의 이름을 구하는 메서드  - 자동차 객체 자신의 전진 위치를 증가시키는 메서드

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,47 @@
+package racingcar.model;
+
+public class Car {
+	private static final int MAX_NAME_LENGTH = 5;
+
+	// 자동차 이름
+	private String name;
+	// 전진 위치
+	private String position = "";
+
+	public Car(final String name) {
+		if (name.length() > MAX_NAME_LENGTH) {
+			throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+		}
+		this.name = name;
+	}
+
+	/**
+	 * 자동차의 전진하는 위치를 한 칸 추가한다.
+	 */
+	public void increasePosition() {
+		this.position += "-";
+	}
+
+	/**
+	 * 자동차의 전진 위치를 구한다.
+	 * @return
+	 */
+	public int fetchCurrentPositionLength() {
+		return position.length();
+	}
+
+	/**
+	 * 자동차의 이름을 구한다.
+	 * @return
+	 */
+	public String fetchCarName() {
+		return name;
+	}
+
+	/**
+	 * 자동차의 현재 전진 상태를 출력한다.
+	 */
+	public void printState() {
+		System.out.println("%s : %s".formatted(name, position));
+	}
+}
