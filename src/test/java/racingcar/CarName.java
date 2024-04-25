13dd5f2b9b959dad0487c577a0ebd29383d15dcc
jjanggu <ssc6839@gmail.com>
2024-04-26 01:03:27 +0900

    feat: 자동차 이름 입력 시 5자 이하인 경우 정상적으로 생성하도록 구현

--- src/test/java/racingcar/CarName.java
+++ src/test/java/racingcar/CarName.java
@@ -2,6 +2,8 @@ package racingcar;
 
 public class CarName {
 
+    private static final int CAR_NAME_LIMIT_LENGTH = 5;
+
     private final String name;
 
     private CarName(final String name) {
@@ -9,6 +11,13 @@ public class CarName {
     }
 
     public static CarName from(final String name) {
-        throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        validate(name);
+        return new CarName(name);
+    }
+
+    private static void validate(final String name) {
+        if (name.length() > CAR_NAME_LIMIT_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
     }
 }
--- src/test/java/racingcar/CarNameTest.java
+++ src/test/java/racingcar/CarNameTest.java
@@ -1,10 +1,16 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
 
 public class CarNameTest {
+
+    @Test
+    void 자동차_이름이_5자_이하인_경우_정상적으로_생성한다() {
+        assertDoesNotThrow(() -> CarName.from("12345"));
+    }
 
     @Test
     void 자동차_이름은_5자를_초과할_수_없다() {
