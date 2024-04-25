857d698ac0fc41536ba265df9d7661dc7ca7fd2d
jjanggu <ssc6839@gmail.com>
2024-04-26 02:47:28 +0900

    refactor: 자동차 이름 입력 시 유효성 검증 로직 클래스 추출

--- src/test/java/racingcar/InputView.java
+++ src/test/java/racingcar/InputView.java
@@ -1,21 +1,14 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
 
-	public static String inputCarNamesToRace() {
+	public static List<String> inputCarNamesToRace() {
 		final String carNames = Console.readLine();
-		if (Objects.isNull(carNames) || carNames.isEmpty()) {
-			throw new IllegalArgumentException("자동차 이름에 빈 값을 입력할 수 없습니다.");
-		}
-		if (carNames.isBlank()) {
-			throw new IllegalArgumentException("자동차 이름에 공백을 입력할 수 없습니다.");
-		}
-		if (carNames.split(",").length == 1) {
-			throw new IllegalArgumentException("경주에 참가할 자동차는 두 대 이상이어야 합니다.");
-		}
-		return carNames;
+		CarNameInputValidator.validate(carNames);
+		return Arrays.stream(carNames.split(",")).toList();
 	}
 }
--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -1,15 +1,16 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputViewTest extends InputTest {
 
-	@EmptySource
+	@ValueSource(strings = {",,짱구카", "짱구카,,흰둥카"})
 	@ParameterizedTest
 	void 자동차_이름_입력_시_빈_값을_입력할_수_없다(final String carNames) {
 		given(carNames);
@@ -19,7 +20,7 @@ public class InputViewTest extends InputTest {
 			.hasMessage("자동차 이름에 빈 값을 입력할 수 없습니다.");
 	}
 
-	@ValueSource(strings = {" ", "   "})
+	@ValueSource(strings = {" , ", "   ,   ", "짱구카, ,흰둥카"})
 	@ParameterizedTest
 	void 자동차_이름_입력_시_공백으로만_이름을_만들_수_없다(final String carNames) {
 		given(carNames);
@@ -29,12 +30,22 @@ public class InputViewTest extends InputTest {
 			.hasMessage("자동차 이름에 공백을 입력할 수 없습니다.");
 	}
 
-	@Test
-	void 한_대의_자동차_이름만_입력할_수_없다() {
-		given("짱구카");
+	@ValueSource(strings = {"짱구카", ",,,"})
+	@ParameterizedTest
+	void 한_대의_자동차_이름만_입력할_수_없다(final String carNames) {
+		given(carNames);
 
 		assertThatThrownBy(InputView::inputCarNamesToRace)
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("경주에 참가할 자동차는 두 대 이상이어야 합니다.");
+	}
+
+	@Test
+	void 정상적인_경우_입력한_자동차_이름_리스트를_반환한다() {
+		given("짱구카,흰둥카");
+
+		final List<String> actual = InputView.inputCarNamesToRace();
+
+		assertThat(actual.size()).isEqualTo(2);
 	}
 }
