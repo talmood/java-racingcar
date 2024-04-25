a4d4997f89dad8443b1c3bac5d2ab762101c5e9d
jjanggu <ssc6839@gmail.com>
2024-04-26 02:16:39 +0900

    feat: 자동차 이름 입력 시 공백만 입력하는 경우 유효성 검사 구현

--- src/test/java/racingcar/InputView.java
+++ src/test/java/racingcar/InputView.java
@@ -7,8 +7,11 @@ public class InputView {
 
 	public static String inputCarNamesToRace() {
 		final String carNames = Console.readLine();
-		if (Objects.isNull(carNames) || carNames.isBlank()) {
+		if (Objects.isNull(carNames) || carNames.isEmpty()) {
 			throw new IllegalArgumentException("자동차 이름에 빈 값을 입력할 수 없습니다.");
+		}
+		if (carNames.isBlank()) {
+			throw new IllegalArgumentException("자동차 이름에 공백을 입력할 수 없습니다.");
 		}
 		return carNames;
 	}
--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputViewTest extends InputTest {
 
@@ -15,5 +16,15 @@ public class InputViewTest extends InputTest {
 		assertThatThrownBy(InputView::inputCarNamesToRace)
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("자동차 이름에 빈 값을 입력할 수 없습니다.");
+	}
+
+	@ValueSource(strings = {" ", "   "})
+	@ParameterizedTest
+	void 자동차_이름_입력_시_공백으로만_이름을_만들_수_없다(final String carNames) {
+		given(carNames);
+
+		assertThatThrownBy(InputView::inputCarNamesToRace)
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("자동차 이름에 공백을 입력할 수 없습니다.");
 	}
 }
