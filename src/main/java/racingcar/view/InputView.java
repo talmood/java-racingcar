4a7c402814c6577b46d29ecd68685f6d31e51d9d
jjanggu <ssc6839@gmail.com>
2024-04-28 17:51:01 +0900

    modify: 입력 시 입력 메세지 출력

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,12 +7,18 @@ import racingcar.controller.request.RoundRequest;
 public class InputView {
 
 	public static CarNamesRequest inputCarNamesToRace() {
+		printInputMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		final String carNames = Console.readLine();
 		return CarNamesRequest.from(carNames);
 	}
 
 	public static RoundRequest inputRacingRound() {
+		printInputMessage("시도할 회수는 몇회인가요?");
 		final String round = Console.readLine();
 		return RoundRequest.from(round);
+	}
+
+	private static void printInputMessage(final String message) {
+		System.out.println(message);
 	}
 }
