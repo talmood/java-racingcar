d931c8fdd724707702c67f91c9a3f37469c1e42f
monsteralover <kmj99301@gmail.com>
2024-04-29 05:42:09 +0900

    feat: application 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,37 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Map;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    private final UserInputCondition userInputCondition;
+    private final Drive drive;
+    private final Score score;
+
+    public Application(UserInputCondition userInputCondition, Drive drive, Score score) {
+        this.userInputCondition = userInputCondition;
+        this.drive = drive;
+        this.score = score;
     }
+
+    public void start() {
+        String[] carNames = userInputCondition.acceptRacingCarNames();
+        int trialTimes = userInputCondition.acceptTrialTimes();
+        Map<String, Integer> mileageForCar = drive.runCarsForTrialTimes(carNames, trialTimes);
+        score.showWinner(mileageForCar);
+
+        Console.close();
+    }
+
+    public static void main(String[] args) {
+
+        UserInputCondition userInput = new UserInput();
+        Drive carDriving = new CarDrive();
+        Score scoreWinner = new ScoreWinner();
+
+        Application application = new Application(userInput, carDriving, scoreWinner);
+        application.start();
+    }
+
 }
