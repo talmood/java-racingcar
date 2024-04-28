8c6be3883584a197f596bdb0f0357994d27795d2
haero77 <shineecard5@gmail.com>
2024-04-28 21:30:34 +0900

    modify: 우승 자동차에 대한 정책 추가 - 우승한 자동차들은 전진 횟수가 동일해야한다.

--- src/main/java/racingcar/model/WinnerCars.java
+++ src/main/java/racingcar/model/WinnerCars.java
@@ -7,7 +7,19 @@ public class WinnerCars {
     private final List<RacingCar> cars;
 
     public WinnerCars(List<RacingCar> cars) {
+        validateWinnerCarPolicy(cars);
         this.cars = cars;
+    }
+
+    private void validateWinnerCarPolicy(final List<RacingCar> cars) {
+        final long distinctNumOfMoveForwardCounts = cars.stream()
+                .mapToInt(RacingCar::getNumOfMovedForward)
+                .distinct()
+                .count();
+
+        if (distinctNumOfMoveForwardCounts > 1) {
+            throw new IllegalArgumentException("All number of moved forward of winner cars must be same.");
+        }
     }
 
     public List<RacingCar> getCars() {
