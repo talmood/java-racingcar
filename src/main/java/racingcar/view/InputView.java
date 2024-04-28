a1b12dead0201892a3bee420a0c9341a57b92bc2
jjanggu <ssc6839@gmail.com>
2024-04-28 12:36:37 +0900

    feat: 경주 라운드 입력 시 0 이하의 값을 입력하는 경우 유효성 검사 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,4 +9,11 @@ public class InputView {
 		final String carNames = Console.readLine();
 		return CarNamesRequest.from(carNames);
 	}
+
+	public static void inputRacingRound() {
+		final int round = Integer.parseInt(Console.readLine());
+		if (round < 1) {
+			throw new IllegalArgumentException("경주 라운드에 0 이하의 값을 입력할 수 없습니다.");
+		}
+	}
 }
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -4,53 +4,76 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.InputTest;
 import racingcar.controller.request.CarNamesRequest;
 
+@DisplayName("입력 테스트")
 public class InputViewTest extends InputTest {
 
-	@ValueSource(strings = {",,짱구카", "짱구카,,흰둥카"})
-	@ParameterizedTest
-	void 자동차_이름_입력_시_빈_값을_입력할_수_없다(final String carNames) {
-		given(carNames);
+	@DisplayName("자동차 이름 입력 시 ")
+	@Nested
+	class CarNameInputTest {
 
-		assertThatThrownBy(InputView::inputCarNamesToRace)
-			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("자동차 이름에 빈 값을 입력할 수 없습니다.");
+		@ValueSource(strings = {",,짱구카", "짱구카,,흰둥카"})
+		@ParameterizedTest
+		void 자동차_이름_입력_시_빈_값을_입력할_수_없다(final String carNames) {
+			given(carNames);
+
+			assertThatThrownBy(InputView::inputCarNamesToRace)
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("자동차 이름에 빈 값을 입력할 수 없습니다.");
+		}
+
+		@ValueSource(strings = {" , ", "   ,   ", "짱구카, ,흰둥카"})
+		@ParameterizedTest
+		void 자동차_이름_입력_시_공백으로만_이름을_만들_수_없다(final String carNames) {
+			given(carNames);
+
+			assertThatThrownBy(InputView::inputCarNamesToRace)
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("자동차 이름에 공백을 입력할 수 없습니다.");
+		}
+
+		@ValueSource(strings = {"짱구카", ",,,"})
+		@ParameterizedTest
+		void 한_대의_자동차_이름만_입력할_수_없다(final String carNames) {
+			given(carNames);
+
+			assertThatThrownBy(InputView::inputCarNamesToRace)
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("경주에 참가할 자동차는 두 대 이상이어야 합니다.");
+		}
+
+		@Test
+		void 정상적인_경우_입력한_자동차_이름_리스트를_반환한다() {
+			given("짱구카,흰둥카");
+
+			final CarNamesRequest actual = InputView.inputCarNamesToRace();
+
+			assertAll(
+				() -> assertThat(actual.getCarNames().size()).isEqualTo(2),
+				() -> assertThat(actual.getCarNames()).containsExactly("짱구카", "흰둥카")
+			);
+		}
 	}
 
-	@ValueSource(strings = {" , ", "   ,   ", "짱구카, ,흰둥카"})
-	@ParameterizedTest
-	void 자동차_이름_입력_시_공백으로만_이름을_만들_수_없다(final String carNames) {
-		given(carNames);
+	@DisplayName("경주 라운드 입력 시 ")
+	@Nested
+	class RoundInput {
 
-		assertThatThrownBy(InputView::inputCarNamesToRace)
-			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("자동차 이름에 공백을 입력할 수 없습니다.");
-	}
+		@ValueSource(strings = {"-1", "0"})
+		@ParameterizedTest
+		void 라운드_입력_시_0_이하의_값을_입력할_수_없다(final String round) {
+			given(round);
 
-	@ValueSource(strings = {"짱구카", ",,,"})
-	@ParameterizedTest
-	void 한_대의_자동차_이름만_입력할_수_없다(final String carNames) {
-		given(carNames);
-
-		assertThatThrownBy(InputView::inputCarNamesToRace)
-			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("경주에 참가할 자동차는 두 대 이상이어야 합니다.");
-	}
-
-	@Test
-	void 정상적인_경우_입력한_자동차_이름_리스트를_반환한다() {
-		given("짱구카,흰둥카");
-
-		final CarNamesRequest actual = InputView.inputCarNamesToRace();
-
-		assertAll(
-			() -> assertThat(actual.getCarNames().size()).isEqualTo(2),
-			() -> assertThat(actual.getCarNames()).containsExactly("짱구카", "흰둥카")
-		);
+			assertThatThrownBy(InputView::inputRacingRound)
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("경주 라운드에 0 이하의 값을 입력할 수 없습니다.");
+		}
 	}
 }
