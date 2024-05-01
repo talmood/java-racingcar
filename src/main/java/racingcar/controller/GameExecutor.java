7aad68b67d4d413cba375b7619de30c038dbef7e
lgm1007 <flxl654@gmail.com>
2024-05-01 11:45:26 +0900

    modify: 게임 시작 시 사용자 안내문 추가

--- src/main/java/racingcar/controller/GameExecutor.java
+++ src/main/java/racingcar/controller/GameExecutor.java
@@ -7,7 +7,9 @@ import racingcar.service.RacingCarGame;
 public class GameExecutor {
 	public static void runGame() {
 		RacingCarGame racingCarGame = new RacingCarGame();
+		System.out.println("경주 할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
 		String carInput = Console.readLine();
+		System.out.println("시도할 회수를 입력하세요.");
 		String countInput = Console.readLine();
 
 		validCountInput(countInput);
